77c0feaaf80c6e53a0d02c4a9bcdbfd912fd82c4
Gil Markham <gmarkham@dropbox.com>
2020-04-02 14:24:06 -0700

    Updated version.

--- build.groovy
+++ build.groovy
@@ -28,8 +28,8 @@ saw.setProperty(Tablesaw.PROP_MULTI_THREAD_OUTPUT, Tablesaw.PROP_VALUE_ON)
 
 programName = "kairosdb"
 //Do not use '-' in version string, it breaks rpm uninstall.
-version = "1.2.1"
-release = saw.getProperty("KAIROS_RELEASE_NUMBER", "1") //package release number
+version = "1.2.1-dd"
+release = saw.getProperty("KAIROS_RELEASE_NUMBER", "2") //package release number
 summary = "KairosDB"
 description = """\
 KairosDB is a time series database that stores numeric values along
